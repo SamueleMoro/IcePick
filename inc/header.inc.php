94af3b09d7b9c986d49053d0dac1cd6e8373b0ee
SamueleMoro <samuelemoro1996@gmail.com>
2017-08-31 21:00:26 +0200

    Nuove pagina incluse nei file .php

---
+++ inc/header.inc.php
@@ -0,0 +1,28 @@
+<header>
+    <div id="menuPrincipale">
+        <div id="wrapper">
+            <div id="menuLeft">
+                <ul class="menu-link">
+                    <li><a href="#" class="active"><i class="fa fa-building-o " aria-hidden="true"></i>Home</a></li>
+                    <li><a href="#"><i class="fa fa-bookmark-o" aria-hidden="true"></i>Categories</a></li>
+                    <li><a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Posts</a></li>
+                    <li><a href="#"><i class="fa fa-desktop " aria-hidden="true"></i>Layouts</a></li>
+                    <li><a href="#"><i class="fa fa-user-o" aria-hidden="true"></i>About me</a></li>
+                    <li><a href="#"><i class="fa fa-envelope-open-o" aria-hidden="true"></i>Contacts</a></li>
+                </ul>
+            </div>
+            <div id="menuRight">
+                <form>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search...">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default" type="submit">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</header>
