b21ac58e355eb9e92a048fcd5a9f48fbee6846bb
SamueleMoro <samuelemoro1996@gmail.com>
2017-09-02 17:31:33 +0200

    Aggiunta del carousel bootstrap

--- index.php
+++ index.php
@@ -18,11 +18,41 @@
                     -->
                     <div class="col-sm-12">
                         <div id="slider">
-                            <div class="image-slider"></div>
+                            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                                <!-- Indicators -->
+                                <ol class="carousel-indicators">
+                                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                                </ol>
+
+                                <!-- Wrapper for slides -->
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img src="images/post.jpg" alt="Los Angeles">
+                                    </div>
+                                    <div class="item">
+                                        <img src="images/slide1.jpg" alt="Chicago">
+                                    </div>
+                                    <div class="item">
+                                        <img src="images/slide1.jpg" alt="New York">
+                                    </div>
+                                </div>
+
+                                <!-- Left and right controls -->
+                                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div class="col-sm-12">
-                        <h1>Last post</h1>
+                        <h1>Travel posts</h1>
                     </div>
                     <div class="col-sm-4">
                         <?php
