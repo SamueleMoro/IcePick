042f247d686cdd43355146b18aab086307b93b87
Foxitive <dev@foxitive.com>
2017-09-02 15:57:24 +0200

    Merge con la fix della index in backend, che cambia l'URL per l'immagine di default.

--- index.php
+++ index.php
@@ -28,57 +28,79 @@
                         <?php
                         $allPosts = [];
                         $nColumns = 3;
-
-                        # Loading post data in the $allPosts array
+                        
+                        //Loading post data in the $allPosts array
                         while(have_posts()) {
                             the_post();
+                            
                             array_push($allPosts, $post);
                         }
                         ?>
+                        
                         <?php
-                            # Posts in column 1
-                            for($i = 0; $i < count($allPosts); $i+=$nColumns) {
+                            //Posts in column 1
+                            for($i = 0; $i < count($allPosts); $i+=$nColumns)
+                            {
                                 include "thumbnail.php";
                             }
                         ?>
                     </div>
                     <div class="col-sm-4">
                         <?php
-                            # Posts in column 2
-                            for($i = 1; $i < count($allPosts); $i+=$nColumns) {
+                            //Posts in column 2
+                            for($i = 1; $i < count($allPosts); $i+=$nColumns)
+                            {
                                 include "thumbnail.php";
                             }
                         ?>
                     </div>
                     <div class="col-sm-4">
                         <?php
-                            # Posts in column 3
-                            for($i = 2; $i < count($allPosts); $i+=$nColumns) {
+                            //Posts in column 3
+                            for($i = 2; $i < count($allPosts); $i+=$nColumns)
+                            {
                                 include "thumbnail.php";
                             }
                         ?>
                     </div>
                 </div>
             </div>
-            <?php require_once 'inc/footer.inc.php'; ?>
+            <div id="socialTab">
+                <table class="link-table">
+                    <tr class="social-link">
+                        <td><a href="#"><i class="fa fa-instagram " aria-hidden="true"></i></a></td>
+                        <td><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></td>
+                        <td><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></td>
+                        <td><a href="#"><i class="fa fa-google-plus " aria-hidden="true"></i></a></td>
+                        <td><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></td>
+                        <td><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></td>
+                    </tr>
+                </table>
+            </div>
+            <footer>
+                <p>&copy; 2017. Ice Pick. All Rights Reserved</p>
+            </footer>
         </div>
+        <!--
+            <a href="" class="menu-hide"><i class="fa fa-bars" aria-hidden="true"></i></a>
+        -->
     </body>
 </html>
 
 <?php
-    # Functions
+    //Functions
 
-    # Returns the URL of the first image in a post.
+    //Returns the URL of the first image in a post.
     function post_thumbnail($post) {
         $first_img = '';
         ob_start();
         ob_end_clean();
         $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
         $first_img = $matches [1] [0];
-
-        # Set a default image if the post doesn't have any images
-        if(empty($first_img)) {
-            $first_img = $icepickDir."/images/default.jpg";
+        
+        //Set a default image if the post doesn't have any images
+        if(empty($first_img)){ 
+            $first_img = get_template_directory_uri()."/images/default.jpg";
         }
         return $first_img;
     }
