c41f44d04b4619c7b773203d78ab319c7f0161c5
SamueleMoro <samuelemoro1996@gmail.com>
2017-09-02 20:45:46 +0200

    Inserito il logo nell'header e spostato alcuni link

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -2,6 +2,11 @@
     <div id="menuPrincipale">
         <div class="container">
             <div id="menuLeft">
+                <div class="logo-web">
+                    <a href="#" class="logo-link">
+                        <img src="<?php bloginfo('template_url'); ?>/images/logo.jpg">
+                    </a>
+                </div>
                 <!--
                 <form action="" method="post">
                     <div class="input-group">
@@ -18,10 +23,10 @@
             <div id="menuRight">
                 <ul class="menu-link">
                     <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">About me</a></li>
+                    <li><a href="#">Layouts</a></li>
                     <li><a href="#">Categories</a></li>
                     <li><a href="#">Posts</a></li>
-                    <li><a href="#">Layouts</a></li>
-                    <li><a href="#">About me</a></li>
                     <li><a href="#">Contacts</a></li>
                 </ul>
             </div>
